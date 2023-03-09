2b156d3bb0f394afb723051fadaaf865107b24cf
abdo <abdo.mohamadeen@gmail.com>
2023-03-09 20:32:03 +0400

    Update background.js to automatically reload on browser focus

--- background.js
+++ background.js
@@ -3,15 +3,28 @@ chrome.runtime.onInstalled.addListener((details) => {
     // Code to be executed on first install
   } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
     // When extension is updated
-  } else if (details.reason === chrome.runtime.OnInstalledReason.CHROME_UPDATE) {
+  } else if (
+    details.reason === chrome.runtime.OnInstalledReason.CHROME_UPDATE
+  ) {
     // When browser is updated
-  } else if (details.reason === chrome.runtime.OnInstalledReason.SHARED_MODULE_UPDATE) {
+  } else if (
+    details.reason === chrome.runtime.OnInstalledReason.SHARED_MODULE_UPDATE
+  ) {
     // When a shared module is updated
   }
 });
 
 chrome.commands.onCommand.addListener((shortcut) => {
-    if(shortcut === 'reload-e') {
-        chrome.runtime.reload();
-    }
-})
+  if (shortcut === 'reload-e') {
+    chrome.runtime.reload();
+  }
+});
+
+// the extension reloads when the browser is focused, only in development
+chrome.windows.onFocusChanged.addListener(() => {
+  const isDevMode = !('update_url' in chrome.runtime.getManifest());
+
+  if (isDevMode) {
+    chrome.runtime.reload();
+  }
+});
